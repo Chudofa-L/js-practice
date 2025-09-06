1c36cbf37d1cd0ea7ba5b19631b87cec40d41c0e
ljuba@Lyba00 <ljuba650@gmail.com>
2025-09-06 16:59:48 +0300

    art-gallery

--- urok-1/app.js
+++ urok-1/app.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlagin(active = 2) {
+  const slides = document.querySelectorAll('.slide');
 
-for (const slide of slides) {
-  slide.addEventListener('click', () => {
-    clearActiveClasses()
-    slide.classList.add('active');
-  })
+  slides[active].classList.add('active')
+
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+      slide.classList.add('active');
+    })
+  };
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('active')
+    })
+  };
 };
 
-function clearActiveClasses() {
-  slides.forEach((slide) => {
-    slide.classList.remove('active')
-  })
-};
+slidesPlagin();
