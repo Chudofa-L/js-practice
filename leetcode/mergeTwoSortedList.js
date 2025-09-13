2dfb33797e7de207c00547c894fb7870790917dc
ljuba@Lyba00 <ljuba650@gmail.com>
2025-09-13 15:31:56 +0300

    leetcode-problems

---
+++ leetcode/mergeTwoSortedList.js
@@ -0,0 +1,13 @@
+// const list1 = [1, 2, 4];
+// const list2 = [1, 3, 4];
+
+// const mergeTwoLists = function (list1, list2) {
+//   if (list1.length === 0 && list2.length === 0) {
+//     return [];
+//   }
+
+//   const list = (list1.join('') + list2.join('')).split('').sort().map(Number);
+//   return console.log(list);;
+// };
+
+// mergeTwoLists(list1, list2)
